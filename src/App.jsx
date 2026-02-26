6ed6f1f1a2280a91bad11f0f7640c74a2f77cadf
Johnson Katti <johnson.katti@gmail.com>
2026-02-26 11:16:00 +0530

    One test one test

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React + Johnson</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is testing for my{count}
+          count is testing for my own{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
