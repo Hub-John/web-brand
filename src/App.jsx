4f9bf9f136a2e6041d3f7c0ca199878cfae1ab43
Johnson Katti <johnson.katti@gmail.com>
2026-02-26 12:43:38 +0530

    test fi

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Johnson</h1>
+      <h1>Vite + React + Priscilla</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count{count}
